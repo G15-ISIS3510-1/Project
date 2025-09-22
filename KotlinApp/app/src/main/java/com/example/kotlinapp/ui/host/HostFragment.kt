9aa9814759148dc9aabf8736cdd00aacb0eb4cf5
Marcosespa <marcosespa24@gmail.com>
2025-09-22 07:43:42 -0500

    Messages with bottom bar and also redirect working

---
+++ KotlinApp/app/src/main/java/com/example/kotlinapp/ui/host/HostFragment.kt
@@ -0,0 +1,31 @@
+package com.example.kotlinapp.ui.host
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.example.kotlinapp.R
+
+class HostFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_host, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val composeView: ComposeView = view.findViewById(R.id.hostCompose)
+        composeView.setContent {
+            HostScreen { pin ->
+            }
+        }
+    }
+}
+
+
