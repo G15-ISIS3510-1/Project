020d349ac939d1914df06acf4683e38886ec4b85
Marcosespa <marcosespa24@gmail.com>
2025-09-21 19:09:25 -0500

    Messages implementend

--- KotlinApp/app/src/main/java/com/example/kotlinapp/ui/home/HomeFragment.kt
+++ KotlinApp/app/src/main/java/com/example/kotlinapp/ui/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.kotlinapp.R
 import com.example.kotlinapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
