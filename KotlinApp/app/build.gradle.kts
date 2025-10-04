7b1a3dfdafa22fefca0b8c51d1f4eef58b2c9b01
JuanPuig10 <j.puig@uniandes.edu.co>
2025-10-04 16:58:19 -0500

    type 2 notification

--- KotlinApp/app/build.gradle.kts
+++ KotlinApp/app/build.gradle.kts
@@ -126,5 +126,9 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("com.google.android.material:material:1.12.0")   
 
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("com.google.android.material:material:1.12.0")
+
 
 }
